08b20a01bbc2a095c2242341c23d7b5fb54beb58
bergloman <bergloman@gmail.com>
2017-06-02 17:59:22 +0200

    testing multiple workers, not done yet

---
+++ db/v3.sql
@@ -0,0 +1,6 @@
+create procedure qtopology_sp_leader_ping(p_name varchar(100))
+begin
+    update qtopology_worker
+    set status = 'alive', lstatus_ts = NOW(), last_ping = NOW()
+    where name = p_name and lstatus = 'leader';
+end;
