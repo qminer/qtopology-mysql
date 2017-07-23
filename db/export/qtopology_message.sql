70e112d177f2e58cc0069c7ab0a7c90f0e5d4dc6
Viktor Jovanoski <viktor@carvic.si>
2017-07-24 01:24:40 +1000

    Added missing sql change

---
+++ db/export/qtopology_message.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `qtopology_message` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `worker` varchar(100) NOT NULL,
+  `cmd` varchar(100) NOT NULL,
+  `content` mediumtext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
