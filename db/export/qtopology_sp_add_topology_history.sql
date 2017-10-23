5fe294ef47e96500b97b7c302b757533e52d7377
Viktor Jovanoski <viktor@carvic.si>
2017-10-23 11:57:32 +0200

    SQL refresh

--- db/export/qtopology_sp_add_topology_history.sql
+++ db/export/qtopology_sp_add_topology_history.sql
@@ -1,7 +1,7 @@
 CREATE  PROCEDURE `qtopology_sp_add_topology_history`(p_uuid varchar(100))
 begin
-  insert into qtopology_topology_history(ts, uuid, status, worker, weight, worker_affinity, error, enabled)
-  select NOW() as ts, uuid, status, worker, weight, worker_affinity, error, enabled
+  insert into qtopology_topology_history(ts, uuid, status, worker, weight, worker_affinity, error, enabled, pid)
+  select NOW() as ts, uuid, status, worker, weight, worker_affinity, error, enabled, pid
   from qtopology_topology
   where uuid = p_uuid;
 end
